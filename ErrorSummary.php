273be94a7f0d6ada30e06b3d2d11ea8ff2942ec4
Pavel <pavel.lance@gmail.com>
2017-04-27 20:38:23 +0300

    Добавлен шаблон оформления

--- ErrorSummary.php
+++ ErrorSummary.php
@@ -4,6 +4,10 @@ namespace plance\widget;
 class ErrorSummary extends \yii\base\Widget
 {
 	public $Model;
+	public $template =	'<div class="alert alert-warning alert-dismissible" role="alert">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							{text}
+						</div>';
 	
     public function run()
     {
--- views/error-summary.php
+++ views/error-summary.php
@@ -1,14 +1,10 @@
 <?php
 use yii\helpers\Html;
-?>
 
-<?php if($Model -> hasErrors()): ?>
-<div class="alert alert-warning alert-dismissible" role="alert">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	<?php
-		echo Html::errorSummary($Model, [
+if($Model -> hasErrors()):
+	echo strtr($this -> context -> template, [
+		'{text}' => Html::errorSummary($Model, [
 			'header' => false
-		]);
-	?>
-</div>
-<?php endif; ?>
+		])
+	]);
+endif;
